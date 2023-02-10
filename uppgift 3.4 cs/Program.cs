7a5c3c26fdcae7b4146473f063aef03cb707eedc
Pingvu <nhlpool27@gmail.com>
2023-02-10 09:06:42 +0100

    Add project files.

---
+++ uppgift 3.4 cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uppgift34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur många minuter lång är din låt??");
+            int min = int.Parse(Console.ReadLine());
+            Console.WriteLine("hur många sekunder?");
+            int sec = int.Parse(Console.ReadLine());
+
+            if (min <= 2 && sec < 45)
+                Console.WriteLine("din låt är för kort");
+            else if (min > 4 && sec > 20)
+                Console.WriteLine("din låt är för lång");
+            else
+            Console.WriteLine("din låt är bra längd för radio");
+        }
+    }
+}
